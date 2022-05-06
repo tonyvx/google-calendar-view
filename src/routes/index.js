214c167789ed2922481baf214e8eadb6cd7c1697
tonyvx <tonyvx@avx007.org>
2022-05-06 11:44:01 -0400

    Cleanup

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,21 +2,23 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 var calendar = require('../services/gCalendar');
+
+var cors = require('cors');
 /* GET home page. */
 router.get(['/calendar/*', '/users', '/about'], function (req, res, next) {
   res.sendFile(path.join(__dirname + '/../public/index.html'));
 });
 
 
-router.get('/:id/events', async function (req, res) {
+router.get('/:id/events', cors(), async function (req, res) {
   return res.send(await calendar.getEvents(req.params.id));
 });
 
-router.get('/:id/events/:from', async function (req, res) {
+router.get('/:id/events/:from', cors(), async function (req, res) {
   return res.send(await calendar.getEvents(req.params.id, req.params.from));
 });
 
-router.get('/:id/events/:from/:to', async function (req, res) {
+router.get('/:id/events/:from/:to', cors(), async function (req, res) {
   return res.send(await calendar.getEvents(req.params.id, req.params.from, req.params.to));
 });
 
