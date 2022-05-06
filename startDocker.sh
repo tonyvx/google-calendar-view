00f4ec517e45986e252faacb7f52a3e8caebccd5
tonyvx <tonyvx@avx007.org>
2022-05-06 13:07:29 -0400

    Add back files for frontend

--- startDocker.sh
+++ startDocker.sh
@@ -1,4 +1,4 @@
 sudo docker stop gcalendar
 sudo docker rm gcalendar
-sudo docker pull tonyvx/gcalendar:1.0
+sudo docker pull tonyvx/gcalendar:1.1
 sudo docker run -d --name gcalendar -p 8181:8181 -e TZ=America/New_York tonyvx/gcalendar:1.1
